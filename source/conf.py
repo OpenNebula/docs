0e33f7010e7f5a78c1be322700f1e6a842e6dbc8
Jaime Melis <jmelis@opennebula.org>
2013-12-12 22:40:40 +0100

    Conf file changes: * enable TODO plugin * render sidebars

--- source/conf.py
+++ source/conf.py
@@ -29,6 +29,8 @@ import sphinx_bootstrap_theme
 # coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.todo']
 
+todo_include_todos = True
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
@@ -133,7 +135,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = {'**': ['alltoc.html', 'sourcelink.html', 'searchbox.html']}
 
 # Additional templates that should be rendered to pages, maps page names to
 # template names.
