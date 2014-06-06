beb95feab00e38f973852377cabfb63447a5fb93
Jaime Melis <jmelis@opennebula.org>
2014-06-06 10:27:27 +0200

    bump version

--- source/conf.py
+++ source/conf.py
@@ -54,7 +54,7 @@ copyright = '2013, OpenNebula Team <contact@opennebula.org>'
 # built documents.
 #
 # The short X.Y version.
-version = '4.6'
+version = '4.8'
 # The full version, including alpha/beta/rc tags.
 release = version
 
