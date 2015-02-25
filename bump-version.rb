90f8ffd78d3de1a553d75b31e95e3ea9243e16d2
Jaime Melis <jmelis@opennebula.org>
2015-02-25 13:38:42 +0100

    add bump-version.rb script

---
+++ bump-version.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+
+require 'colored'
+
+if ARGV.length != 2
+    STDERR.puts "Usage: #{$0} <old_version> <new_version>"
+end
+
+old_version, new_version = ARGV
+
+Dir.chdir File.dirname(__FILE__)+"/source"
+
+Dir['**/*'].each do |file|
+    next unless File.file? file
+
+    skip = false
+    %w(_ conf.py release_notes/ locale/ images/ ext/ toc.html).each do |d|
+        if file.start_with?(d)
+            skip = true
+            break
+        end
+    end
+    next if skip
+
+    text = File.read(file)
+    text_original = text.clone
+
+    text.gsub!(old_version, new_version)
+
+    if text != text_original
+        puts "* Changing: #{file}"
+        File.open(file,'w'){|f| f.write(text)}
+    end
+end
+
+puts
+puts "*** Remember to update conf.py and the release_notes***"
+puts
