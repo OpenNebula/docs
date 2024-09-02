d7347e39750877100b9c4641c6e860f5d1fdb12b
Tino Vázquez <cvazquez@opennebula.io>
2024-09-02 17:01:41 +0200

    Revert "M #-: Bump version to 6.99.0"
    This reverts commit e5768608d6cb4e062999f9ab44092132fd33342b.

--- source/conf.py
+++ source/conf.py
@@ -86,9 +86,9 @@ copyright = '2024, OpenNebula Team <contact@opennebula.io>'
 # built documents.
 #
 # The short X.Y version.
-version = '7.0'
+version = '6.10'
 # The full version, including alpha/beta/rc tags.
-release = '6.99.0'
+release = '6.10.0'
 # The context packages released version
 context_release = '6.10.0'
 
