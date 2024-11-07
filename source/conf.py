9e85f00a5fe64e0567ee04accd0e41fd4e07b25a
Tino Vázquez <cvazquez@opennebula.io>
2024-11-07 13:02:11 +0100

    M #-: Bump 6.10.1

--- source/conf.py
+++ source/conf.py
@@ -88,7 +88,7 @@ copyright = '2024, OpenNebula Team <contact@opennebula.io>'
 # The short X.Y version.
 version = '6.10'
 # The full version, including alpha/beta/rc tags.
-release = '6.10.0'
+release = '6.10.1'
 # The context packages released version
 context_release = '6.10.0'
 
